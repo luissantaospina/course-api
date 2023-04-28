f8cf85391e222526065f18724d1d3e775ff4268f
Luis <luisfernandosantaospina@gmail.com>
2023-04-28 15:55:55 -0500

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,9 @@ use App\Http\Controllers\Course\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'v1'], function () {
-    Route::group(['prefix' => 'auth'], function () {
-        Route::get('courses', [CourseController::class, 'index']);
-        Route::post('courses', [CourseController::class, 'store']);
-        Route::get('course/{id}', [CourseController::class, 'show']);
-        Route::delete('course/{id}', [CourseController::class, 'destroy']);
-        Route::put('course/{id}', [CourseController::class, 'update']);
-    });
+    Route::get('courses', [CourseController::class, 'index']);
+    Route::post('courses', [CourseController::class, 'store']);
+    Route::get('course/{id}', [CourseController::class, 'show']);
+    Route::delete('course/{id}', [CourseController::class, 'destroy']);
+    Route::put('course/{id}', [CourseController::class, 'update']);
 });
